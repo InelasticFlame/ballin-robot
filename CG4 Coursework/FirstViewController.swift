9fd9f849d8f48c42b2190849df853d9328f29749
William Ray <williamray@Williams-MBP.home>
2014-12-22 21:36:18 +0000

    Initial Commit

---
+++ CG4 Coursework/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CG4 Coursework
+//
+//  Created by William Ray on 22/12/2014.
+//  Copyright (c) 2014 William Ray. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CG4 CourseworkTests/CG4_CourseworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  CG4_CourseworkTests.swift
+//  CG4 CourseworkTests
+//
+//  Created by William Ray on 22/12/2014.
+//  Copyright (c) 2014 William Ray. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CG4_CourseworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
