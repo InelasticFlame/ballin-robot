11d02f280d509fd5fae58c196189199dd5fd41de
William Ray <wor15@ic.ac.uk>
2019-05-06 15:11:11 +0100

    small clean up

--- Fit - Run Tracker/PersonalBests/PersonalBestTableViewCell.swift
+++ Fit - Run Tracker/PersonalBests/PersonalBestTableViewCell.swift
@@ -14,16 +14,4 @@ class PersonalBestTableViewCell: UITableViewCell {
     @IBOutlet weak var personalBestDescLabel: UILabel!
     @IBOutlet weak var personalBestValueLabel: UILabel!
 
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
