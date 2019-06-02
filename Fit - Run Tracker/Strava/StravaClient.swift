74fbd412619429d636d9c56ec5f72fed31f93f5d
William Ray <wor15@ic.ac.uk>
2019-06-02 15:37:15 +0100

    Create dev StravaClient

---
+++ Fit - Run Tracker/Strava/StravaClient.swift
@@ -0,0 +1,29 @@
+//
+//  StravaClient.swift
+//  Fit - Run Tracker
+//
+//  Created by William Ray on 02/06/2019.
+//  Copyright © 2019 William Ray. All rights reserved.
+//
+
+protocol StravaClient {
+
+    var isAuthorised: Bool { get }
+
+    func authorise()
+    func loadRuns() -> [Run]
+
+}
+
+class DevStravaClient: StravaClient {
+    var isAuthorised: Bool = true
+
+    func authorise() {
+        // no-op
+    }
+
+    func loadRuns() -> [Run] {
+        return [Run(runID: 1, distance: 15.4, dateTime: NSDate.init(timeIntervalSinceNow: 0), pace: 540, duration: 8316, shoe: nil, runScore: 0.0, runLocations: nil, splits: nil)]
+    }
+
+}
